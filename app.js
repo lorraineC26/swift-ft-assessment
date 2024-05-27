00fb3dcff815e1c79366238cb17a6dd3028d3e79
lorraineC26 <lorraine.quinyee@gmail.com>
2024-05-27 15:53:09 -0400

    add button option to upload

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-// converted from app.txt
 const app = Vue.createApp({
   data() {
-    return { documents: [] }
+    return { documents: [], fileName: '', pdfUrl: null }
+  },
+  methods: {
+    handleDragOver(event) {
+      event.preventDefault()
+      console.log('Drag over event:', event) // Check if dragover event is captured
+    },
+
+    handleDrop(event) {
+      event.preventDefault()
+      const file = event.dataTransfer.files[0]
+      console.log('File dropped:', file.name) // debugging
+      this.fileName = file.name
+      this.pdfUrl = URL.createObjectURL(file)
+    },
+
+    uploadDoc() {
+      this.documents.push(this.fileName)
+      this.fileName = ''
+    }
+
+
   }
 })
 
-app.mount('#user-documents')
+app.mount('#user-docs')
