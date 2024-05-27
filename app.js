db94edb6d174a8b01f6765780148ddd6ce1d8e9d
lorraineC26 <lorraine.quinyee@gmail.com>
2024-05-27 18:16:30 -0400

    update gitignore

--- app.js
+++ app.js
@@ -9,30 +9,30 @@ const app = Vue.createApp({
       // prevent the browser from opening the file
       event.preventDefault()
 
-      const file = event.dataTransfer.files[0]
+      const file = event.dataTransfer.files[0]; // access the data being dragged and access the first file
 
-      this.fileName = file.name
-      this.pdfUrl = URL.createObjectURL(file)
+      this.fileName = file.name;
+      this.pdfUrl = URL.createObjectURL(file);
     },
 
     uploadDoc() {
       // only allow uploading if there is a file name
       if (this.fileName !== '') {
-        this.documents.push(this.fileName)
-        this.fileName = '' // reset the file name after uploading
+        this.documents.push(this.fileName);
+        this.fileName = ''; // reset the file name after uploading
       }
     },
 
     // remove the document from the documents array
     deleteDoc(index) {
-      this.documents.splice(index, 1)
+      this.documents.splice(index, 1);
     },
 
     handleFileSelect(event) {
-      const file = event.target.files[0]
+      const file = event.target.files[0];
 
-      this.fileName = file.name
-      this.pdfUrl = URL.createObjectURL(file)
+      this.fileName = file.name;
+      this.pdfUrl = URL.createObjectURL(file);
     }
   }
 })
